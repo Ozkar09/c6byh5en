88fc9a4e4c189dbde8123961a1e58b808065d1ed
Ozkar09 <oscarbeltran99@hotmail.com>
2019-08-08 21:37:14 -0500

    Se hacen pruebas para solucionar incidencia

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -26,7 +26,6 @@ class Api::V1::ProductsController < ApplicationController
 
   def update
     @product = Product.find(params[:id])
-    respond_to :json
       if @product.update(product_params)
         render json: @product.to_json, head: 200, nothing: true
       else
