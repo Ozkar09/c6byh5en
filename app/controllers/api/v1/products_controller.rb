047ab43187f9eb4905af6bda7f8a08ecb4c9ecd3
Ozkar09 <oscarbeltran99@hotmail.com>
2019-08-08 19:54:48 -0500

    Se corrige error de formato

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -28,7 +28,7 @@ class Api::V1::ProductsController < ApplicationController
     @product = Product.find(params[:id])
     respond_to :json
       if @product.update(product_params)
-        render json: @product.to_json, status: 200
+        render status: 200
       else
         render json: @product.errors.full_messages, status: 422
       end
