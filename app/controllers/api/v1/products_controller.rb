12aeaba2f931074f01558de06fa22eb1a17fcf02
Ozkar09 <oscarbeltran99@hotmail.com>
2019-08-08 19:59:08 -0500

    Se dejan solo los metodos requeridos para hacer pruebas

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,34 +1,10 @@
 class Api::V1::ProductsController < ApplicationController
-
-  def index
-    @products = Product.all
-    render json: @products.as_json
-  end
-
-  def new
-    @product = Product.new
-  end
-
-  def create
-    @product = Product.new(product_params)
-    respond_to do |format|
-      if @product.save
-        format.json { render json: @product, status: :created, location: @product }
-      else
-        format.json { render json: @product.errors.full_messages, status: 422}
-      end
-    end
-  end
-
-  def edit
-    @product = Product.find(params[:id])
-  end
-
+  
   def update
     @product = Product.find(params[:id])
     respond_to :json
       if @product.update(product_params)
-        render status: 200
+        render json: @product.to_json, status: 200
       else
         render json: @product.errors.full_messages, status: 422
       end
