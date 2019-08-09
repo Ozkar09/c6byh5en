721f47906a74ad4df81382c8cdc4c6fad32047a2
Ozkar09 <oscarbeltran99@hotmail.com>
2019-08-08 21:18:52 -0500

    Se hacen pruebas para solucionar incidencia

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,13 +1,48 @@
 class Api::V1::ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+    render json: @products.as_json
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    respond_to do |format|
+      if @product.save
+        format.json { render json: @product, status: :created, location: @product }
+      else
+        format.json { render json: @product.errors.full_messages, status: 422}
+      end
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
   def update
     @product = Product.find(params[:id])
     respond_to :json
       if @product.update(product_params)
-        render json: @product.to_json, status: 200, location: @product
+        render json: @product.to_json, head: 200
       else
-        #render json: @product.errors.full_messages, status: 422
+        render json: @product.errors.full_messages, status: 422
       end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    respond_to do |format|
+      if @product.destroy
+        format.json { render json: @product, head: 204, location: @product }
+      else
+        format.json { render json: @product.errors.full_messages, status: 422}
+      end
+    end
   end
 
   private
